d7e60f603c0cfc4e6177e4d1f072a0ac7ad29f0e
HELIO POTELICKI <helio.p2000@hotmail.com>
2020-11-23 17:37:00 -0300

    fix distance

--- Q1/distancia.py
+++ Q1/distancia.py
@@ -8,15 +8,15 @@ def saveCsv(nomearquivo, arquivo):
 
 
 df = pd.read_csv('Q1\entry.csv', sep=';')
-distance = []
+distances = []
 for i in range(0, len(df)):
     x1 = int(df['X1'][i])
     y1 = int(df['Y1'][i])
     x2 = int(df['X2'][i])
     y2 = int(df['Y2'][i])
     res = m.sqrt(((x2 - x1)**2) + ((y2 - y1)**2))
-    distance.append(f'{res:.8f}')
-df['Distancia'] = distance
+    distances.append(f'{res:.8f}')
+df['Distancia'] = distances
 
 saveCsv('exit', df)
 print('saved')
