4d13d88185db95e59b4a8b32626ef8d3b119d7e2
HELIO POTELICKI <helio.p2000@hotmail.com>
2020-11-23 16:38:43 -0300

    Calcula distancia

---
+++ Q1/distancia.py
@@ -0,0 +1,22 @@
+import math as m
+import pandas as pd
+
+
+def saveCsv(nomearquivo, arquivo):
+    save = (f'Q1\{nomearquivo}.csv')
+    arquivo.to_csv(save, sep=',', index=True)
+
+
+df = pd.read_csv('Q1\entry.csv', sep=';')
+distance = []
+for i in range(0, len(df)):
+    x1 = int(df['X1'][i])
+    y1 = int(df['Y1'][i])
+    x2 = int(df['X2'][i])
+    y2 = int(df['Y2'][i])
+    res = m.sqrt(((x2 - x1)**2) + ((y2 - y1)**2))
+    distance.append(f'{res:.8f}')
+df['Distancia'] = distance
+
+saveCsv('exit', df)
+print('saved')
